ca716185cfee0680b5966ac71e72065bea97697e
Curtis Crawford <curtiscraw@gmail.com>
2016-01-22 02:26:23 -0800

    Initial setup scrip commit, needs testing

---
+++ python/initial_uart_setup.py
@@ -0,0 +1,7 @@
+import Adafruit_BBIO.UART as UART
+
+#enable all relevant UART ports for gps and xbee
+#UART.setup("UART0")
+UART.setup("UART1")
+#UART.setup("UART2")
+#UART.setup("UART3")
---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#used to setup the beaglebone environment after a fresh clone of this repo
+
+#ONLY RUN ON A BEAGLEBON RUNNING DEBIAN
+
+WORKING_DIR=$PWD
+
+#ensure root permisions
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+#setup the required Debian packages
+apt-get update
+apt-get upgrade
+apt-get install -y git gpsd gpsd-clients python-gps python python-dev python-setuptools build-essential python-smbus
+
+#install pip modules
+pip install pyserial
+pip install Adafruit_BBIO
+
+#instantiate the needed git submodules
+git pull --recurse-submodules
+git submodule update --recursive
+
+#install Adafruit altimeter module 
+cd Adafruit_Python_BMP
+python setup.py install
+cd $WORKING_DIR
+
+#TODO create the install script for LSM9DS0 and BMP180 wrappers
+
+#install the Accelerometer module
+#cd LSM9DS0_Python_lib
+#python setup.py install
+#cd $WORKING_DIR
+
+#setup the gpsd to listen to the correct USART port
+cd python
+#gpsd -n /dev/ttyO0 -F /var/run/gpsd.sock
+python initial_uart_setup.py
+gpsd -n /dev/ttyO1 -F /var/run/gpsd.sock
+#gpsd -n /dev/ttyO2 -F /var/run/gpsd.sock
+#gpsd -n /dev/ttyO3 -F /var/run/gpsd.sock
+cd $WORKING_DIR
+
+exit
