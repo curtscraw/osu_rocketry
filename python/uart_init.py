aed2146c79d11a83ae56ea9055095863a1f0bcc6
root <root@beaglebone.localdomain>
2016-01-27 07:48:57 +0000

    Properly init uart with simple call

--- python/uart_init.py
+++ python/uart_init.py
@@ -1,6 +1,7 @@
 import Adafruit_BBIO.UART as UART
 
-def init_uart1( ):
-	"initialize the BBB UART1 interface for the GPS"
-	UART.setup("UART1")
+print "Initialize UART1 for XBEE"
+UART.setup("UART1")
 
+print "Initialize UART2 for GPS"
+UART.setup("UART2")
