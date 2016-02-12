80c8aeb81521683388d3a451f15c27bfa008ce2e
root <root@beaglebone.localdomain>
2016-02-12 05:03:50 +0000

    fix the i2c check scriptw

--- bash/check_i2c.sh
+++ bash/check_i2c.sh
@@ -1,25 +1,25 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-GREP_FIND=`i2cdetect -y -r 1 | grep -q 77`
+GREP_FIND=`i2cdetect -y -r 1 | grep 77`
 
 if $GREP_FIND; then 
-  echo "BMP180 found"
-else
   echo "NO BMP180 found"
+else
+  echo "BMP180 found"
 fi
 
-GREP_FIND=`i2cdetect -y -r 1 | grep -q 6b`
+GREP_FIND2=`i2cdetect -y -r 1 | grep 6b`
 
-if $GREP_FIND; then 
-  echo "LSM9DS0 GYRO found"
-else
+if $GREP_FIND2; then 
   echo "NO GYRO found"
+else
+  echo "LSM9DS0 GYRO found"
 fi
 
-GREP_FIND=`i2cdetect -y -r 1 | grep -q 1d`
+GREP_FIND3=`i2cdetect -y -r 1 | grep 1d`
 
-if $GREP_FIND; then 
-  echo "LSM9DS0 ACCELEROMETER found"
-else
+if $GREP_FIND3; then 
   echo "NO ACCELEROMETER found"
+else
+  echo "LSM9DS0 ACCELEROMETER found"
 fi
