1d7d8e946063164fe374bbc4240913bd9c63775e
root <root@beaglebone.localdomain>
2016-02-12 05:28:51 +0000

    Nathan's

--- python/altimetertest.py
+++ python/altimetertest.py
@@ -51,6 +51,6 @@ Po = sensor.read_sealevel_pressure(INIT_ALT) 	#must calibrate using a known itia
 while True:
 	print datetime.datetime.utcnow()
 	print 'Temp = {0:0.2f} *C'.format(sensor.read_temperature())
-	#print 'Pressure = {0:0.2f} Pa'.format(sensor.read_pressure())
+	print 'Pressure = {0:0.2f} Pa'.format(sensor.read_pressure())
 	print 'Altitude = {0:0.2f} m'.format(osu_aiaa_alt(sensor))
-	sleep(0.5);
+	sleep(2);
--- python/cutter_test.py
+++ python/cutter_test.py
@@ -1,6 +1,6 @@
 import Adafruit_BBIO.GPIO as GPIO
 from time import sleep
-from system import exit
+#from system import exit
 
 CUTTER_PIN = "P9_12"
 cutter_delay = 10
@@ -11,7 +11,7 @@ GPIO.output(CUTTER_PIN, GPIO.LOW)
 while cutter_delay > 0:
   print "cutting in: " + str(cutter_delay)
   cutter_delay -= 1
-
+  sleep(1)
 print "Starting the cut"
 GPIO.output(CUTTER_PIN, GPIO.HIGH)
 print "Finished setting cut pin to \"high\""
@@ -19,6 +19,7 @@ print "Finished setting cut pin to \"high\""
 sleep(2)
 
 print "did it cut?"
+GPIO.output(CUTTER_PIN, GPIO.LOW)
 
 GPIO.cleanup()
 
