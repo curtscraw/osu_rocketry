cb6460a447de03afb029d51356f246fb69acf063
root <root@beaglebone.localdomain>
2016-02-12 06:45:38 +0000

    fix to gyro constant issue in a test

--- python/gyro_polling_test.py
+++ python/gyro_polling_test.py
@@ -1,16 +1,16 @@
 import LSM9DS0
 from time import sleep
-from datetime.datetime import utcnow
 import sys
+import datetime
 
-gyro = LSM9DS0.LSM9DS0_GYRO(LSM9DS0_GYRODR_95HZ | LSM9DS0_GYRO_CUTOFF_1, LSM9DS0_GYROSCALE_2000DPS)
+gyro = LSM9DS0.LSM9DS0_GYRO(LSM9DS0.LSM9DS0_GYRODR_95HZ | LSM9DS0.LSM9DS0_GYRO_CUTOFF_1, LSM9DS0.LSM9DS0_GYROSCALE_2000DPS)
 
 while True:
   try:
     (x, y, z) = gyro.read()
-    t = utcnow()
+    t = datetime.datetime.utcnow()
 
-    print t + "x: " + x + " y: " + y + " z: " + z
+    print str(t) + "x: " + str(x) + " y: " + str(y) + " z: " + str(z)
   except KeyboardInterrupt:
     sys.exit()
 
